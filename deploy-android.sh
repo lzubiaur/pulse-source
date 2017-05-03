4a7ed8eeef01c92119c5814a8286d66082754ab8
Laurent Zubiaur <lau.zubiaur@gmail.com>
2017-05-03 10:07:18 +0900

    Add log filter

--- deploy-android.sh
+++ deploy-android.sh
@@ -1,6 +1,11 @@
+# Create the game zip. Must not contain the root folder
 zip -r build/game.love game.lua	main.lua resources conf.lua entities gamestates modules
 
+# Upload the app on the sdcard
 adb push build/game.love /sdcard/game.love
-adb shell am start -S -n "org.love2d.android/.GameActivity" -d "file:///sdcard/game.love"
-# adb logcat
 
+# Start the app Love passing the game file as parameter
+adb shell am start -S -n "org.love2d.android/.GameActivity" -d "file:///sdcard/game.love"
+
+# Show application log for the tag "SDL/APP" level "V" (verbose) and silence all other tag (*:S)
+adb logcat "SDL/APP":D *:S
