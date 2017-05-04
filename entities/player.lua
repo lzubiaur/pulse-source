8213f203f98cd09f1ec28844f32002cc439a22fe
Laurent Zubiaur <lau.zubiaur@gmail.com>
2017-05-05 08:00:52 +0900

    Add basic jump

--- entities/player.lua
+++ entities/player.lua
@@ -5,10 +5,17 @@ local Entity = require 'entities.entity'
 local Player = Class('Player', Entity)
 
 function Player:initialize(world, x,y)
-  self.vx,self.vy = 200,200
-  self.world = world
-  self.x,self.y, self.w, self.h = x,y, conf.cellSize, conf.cellSize
+  Lume.extend(self, {
+      world = world,
+      x = x, y = y,
+      w = conf.cellSize, h = conf.cellSize,
+      vx = 500, vy = 1000, -- gravity
+      jumps = 0, -- jump count (max 2)
+      force = 0, -- jump force
+      released = true, -- touch/key has been released
+  })
   world:add(self, x,y ,self.w, self.h)
+  -- self.jumpTween = Tween.new(0.5, self, { jumpForce = -800 }, 'inOutSine')
 end
 
 function Player:draw()
@@ -19,12 +26,31 @@ function Player:draw()
 end
 
 function Player:update(dt)
-  if love.keyboard.isDown('space') then
-    self.vy = - self.vy
+  if self.force < 0 then
+    self.force = self.force + 500
+    if self.force >= 0 then
+      self.force = 0
+    end
   end
-  self.y = self.y + dt * self.vy
+
+  if love.keyboard.isDown('space') then
+    if self.released and self.jumps < 2 then
+      self.force = -4000
+      self.jumps = self.jumps + 1
+    end
+    self.released = false
+  else
+    self.released = true
+  end
+
+  self.y = self.y + dt * (self.vy + self.force)
   self.x = self.x + dt * self.vx
+
   self.x, self.y, cols, len = self.world:move(self, self.x,self.y)
+
+  if len > 0 and self.jumps > 0 then
+    self.jumps = self.jumps - 1
+  end
 end
 
 return Player
