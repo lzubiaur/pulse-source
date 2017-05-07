37c37613b227523eb62322135ce59f959622c12b
Laurent Zubiaur <lau.zubiaur@gmail.com>
2017-05-08 08:21:04 +0900

    Some improvements

--- resources/shaders/shader1.fs
+++ resources/shaders/shader1.fs
@@ -3,14 +3,14 @@
 // Uniform (extern) variable can't be initialized on Android
 extern number shift;
 
-vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords)
+//vec2 scale = vec2(1.0/800.0, 1.0/600.0);
+vec2 scale = vec2(0.00125, 0.0016);
+
+vec4 effect(vec4 color, Image texture, vec2 tc, vec2 pixel_coords)
   {
-  vec2 tc = texture_coords;
-  vec2 scale = vec2(1.0/800.0, 1.0/600.0);
   vec4 r = Texel(texture, vec2(tc.x + shift * scale.x, tc.y - shift * scale.y));
   vec4 g = Texel(texture, vec2(tc.x, tc.y + shift*scale.y));
   vec4 b = Texel(texture, vec2(tc.x - shift*scale.x, tc.y - shift*scale.y));
-  number a = r.a+g.a+b.a / 3.0;
 
-  return vec4(r.r, g.g, b.b, a);
+  return vec4(r.r, g.g, b.b, r.a+g.a+b.a / 3.0);
 }
