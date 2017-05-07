de51ef2d8d6c367348edb097bd140fdb7cba6f44
Laurent Zubiaur <lau.zubiaur@gmail.com>
2017-05-07 11:33:41 +0900

    Remove deprecated code

--- gamestates/start.lua
+++ gamestates/start.lua
@@ -5,16 +5,13 @@ local Game = require 'game'
 local Start = Game:addState('Start')
 
 function Start:draw()
-  Push:apply('start')
+  Push:start()
 
   Push:setCanvas('shader')
   love.graphics.rectangle('line',0,0,conf.width,conf.height)
-  -- love.graphics.draw(image1, (gameWidth-image1:getWidth())*.5, (gameHeight-image1:getHeight())*.5 - 100) --global shader + canvas shader will be applied
-  love.graphics.rectangle('line', 100,100, 40,40)
   love.graphics.print('Push key to start',100,100)
-
-  Push:setCanvas('noshader')
-  Push:apply('end')
+  -- Push:setCanvas('noshader')
+  Push:finish()
 end
 
 function Start:update(dt)
