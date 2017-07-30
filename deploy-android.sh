e1d8bc5f3bb54f754b9e903a9f06577fd1f746d4
lzubiaur <zubiaur.laurent@gmail.com>
2017-07-30 07:42:40 +0200

    Use gradle

--- deploy-android.sh
+++ deploy-android.sh
@@ -1,45 +1,75 @@
 #!/bin/sh
 
-LOVE_ANDROID=../love-10.2-android-source
+# Build the Android apk in debug/release mode or deploy the game to the Love2d app (from the play store)
 
-if [ -z "$1" -o "$1" != "debug" -a "$1" != "release" ]; then
-  echo "ERROR: '$1' is not valid. Either 'debug' or 'release' is required."
-  exit -1
-fi
+LOVE_ANDROID=../love-android-sdl2
+
+# debug/release
+BUILD=debug
+# e.g. com.mycompany.myproject
+PACKAGE=com.voodoocactus.games.pulse
+
+FILES="build.lua
+version.lua 
+game.lua
+entities
+gamestates
+modules
+resources
+conf.lua
+main.lua
+"
 
 # Write the debug/release configuration
-echo "return '$1'" > build.lua
-# Create the game zip. Must not contain the root folder
-rm $LOVE_ANDROID/assets/game.love
-zip -r $LOVE_ANDROID/assets/game.love build.lua version.lua game.lua main.lua resources conf.lua entities gamestates modules -x *.DS_Store
+echo "return '$BUILD'" > common/build.lua
 
 if [ "$1" == "app" ]; then
+  mkdir -p build
+  rm build/game.love
+  zip -r build/game.love $FILES -x *.DS_Store
   # Unlock the screen
   adb shell input keyevent 82
   adb shell input swipe 100 100 800 200
 
-  adb shell am force-stop "org.love2d.android"
+  adb shell am force-stop "$PACKAGE"
 
   # Upload the app on the sdcard
   adb push build/game.love /sdcard/game.love
 
   # Start the app Love passing the game file as parameter
-  adb shell am start -S -n "org.love2d.android/.GameActivity" -d "file:///sdcard/game.love"
+  adb shell am start -S -n "$PACKAGE/.GameActivity" -d "file:///sdcard/game.love"
   # Show application log for the tag "SDL/APP" level "V" (verbose) and silence all other tag (*:S)
   adb logcat "SDL/APP":D *:S
   exit 0
 fi
 
-pushd ../love-10.2-android-source
+# Create the game zip. Must not contain the root folder
+rm $LOVE_ANDROID/app/src/main/assets/game.love
+zip -r $LOVE_ANDROID/app/src/main/assets/game.love $FILES -x *.DS_Store
 
+pushd $LOVE_ANDROID
+
+# Old way using Ant
 # Build the native framework
 # ndk-build clean
 # ndk-build
-
 # Build the APK and install/start the app
-ant clean && ant $1 && \
-adb uninstall org.love2d.android && \
-adb install "bin/love-android-$1.apk" && \
-adb shell am start -n "org.love2d.android/.GameActivity" && \
+# ant clean && ant $BUILD && \
+# adb uninstall $PACKAGE && \
+# adb install "bin/love-android-$BUILD.apk" && \
+# adb shell am start -n "$PACKAGE/.GameActivity" && \
+# adb logcat "SDL/APP":D *:S
+
+# Clean the project
+# gradlew clean
+gradlew uninstallAll
+if [ "$build" == "release" ]; then
+  gradlew installRelease
+else
+  gradlew installDebug
+fi
+# Start the app
+adb shell am start -n "$PACKAGE/.GameActivity" && \
 adb logcat "SDL/APP":D *:S
+
 popd
